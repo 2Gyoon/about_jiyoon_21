2465cd2ca93c84798b279d4f50e345e5b016d272
2Gyoon <2gyoon52@naver.com>
2021-08-25 01:13:47 +0900

    0824

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const portSlide = new Swiper('.portfolio .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  navigation:{
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev'
+  }
+
+});
